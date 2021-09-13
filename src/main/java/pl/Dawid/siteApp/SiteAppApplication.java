40a31fbdee410b1299288551618a78a767562b92
Dawid <dawid.990m@gmail.com>
2021-09-13 18:07:43 +0200

    first commit

---
+++ src/main/java/pl/Dawid/siteApp/SiteAppApplication.java
@@ -0,0 +1,13 @@
+package pl.Dawid.siteApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteAppApplication.class, args);
+	}
+
+}
